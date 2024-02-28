ab67949869c6c10eb7447f6fb0e33930b52301c6
Alexander Medved <medved1991alexander@gmail.com>
2024-02-28 16:53:43 +0300

    Events & Events Handler

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+
+
+document.querySelector('.check')
+.addEventListener('click', function() {
+    const guessingNumber = Number(document.querySelector('.number-input').value);
+
+    if(!guessingNumber){
+       document.querySelector('.guess-message').textContent = 'Введите число!' 
+    }
+});
