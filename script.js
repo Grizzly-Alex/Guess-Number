5a542b923d1c6f83b779d6534844b7c04803a203
Alexander Medved <medved1991alexander@gmail.com>
2024-02-28 17:23:04 +0300

    Add logic

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 'use strict';
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector('.score').textContent = score;
+document.querySelector('.question').textContent = secretNumber;
 
 
 document.querySelector('.check')
@@ -9,4 +13,22 @@ document.querySelector('.check')
     if(!guessingNumber){
        document.querySelector('.guess-message').textContent = 'Введите число!' 
     }
+    else if(secretNumber == guessingNumber){
+        document.querySelector('.guess-message').textContent = 'Правильно!' 
+    }
+    else if (score > 1){
+        if(guessingNumber > secretNumber){
+            document.querySelector('.guess-message').textContent = 'Слишком много!'
+        }
+        else if(guessingNumber < secretNumber){
+            document.querySelector('.guess-message').textContent = 'Слишком мало!'
+        }
+        score --;
+        document.querySelector('.score').textContent = score;
+    }
+    else{
+        document.querySelector('.score').textContent = 0;
+        document.querySelector('.guess-message').textContent = 'Игра окончена!';
+    }
+
 });
