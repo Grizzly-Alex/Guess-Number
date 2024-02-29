faddaacbea2146f554143b9b1a5c643e7c3f5817
Alexander Medved <medved1991alexander@gmail.com>
2024-02-29 11:02:55 +0300

    CSS manipulation

--- script.js
+++ script.js
@@ -1,27 +1,40 @@
 'use strict';
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highScore = 0;
 document.querySelector('.score').textContent = score;
-document.querySelector('.question').textContent = secretNumber;
+
 
 
 document.querySelector('.check')
 .addEventListener('click', function() {
     const guessingNumber = Number(document.querySelector('.number-input').value);
-
+    //No input
     if(!guessingNumber){
-       document.querySelector('.guess-message').textContent = 'Введите число!' 
+       document.querySelector('.guess-message').textContent = 'Введите число!'; 
     }
+    //Player won
     else if(secretNumber == guessingNumber){
-        document.querySelector('.guess-message').textContent = 'Правильно!' 
-    }
-    else if (score > 1){
-        if(guessingNumber > secretNumber){
-            document.querySelector('.guess-message').textContent = 'Слишком много!'
+        document.querySelector('.question').textContent = secretNumber;
+        document.querySelector('.guess-message').textContent = 'Правильно!';
+        document.querySelector('body').style.backgroundColor = 'rgb(9, 150, 21)';
+        document.querySelector('.question').style.width = '50rem';
+
+        if (score > highScore){
+            highScore = score;
+            document.querySelector('.highScore').textContent = highScore;
         }
+    }
+    //miss
+    else if (score > 1){
+        //To high
+        if(guessingNumber > secretNumber){
+            document.querySelector('.guess-message').textContent = 'Слишком много!';
+        }
+        //To low
         else if(guessingNumber < secretNumber){
-            document.querySelector('.guess-message').textContent = 'Слишком мало!'
+            document.querySelector('.guess-message').textContent = 'Слишком мало!';
         }
         score --;
         document.querySelector('.score').textContent = score;
@@ -31,4 +44,15 @@ document.querySelector('.check')
         document.querySelector('.guess-message').textContent = 'Игра окончена!';
     }
 
+});
+
+document.querySelector('.again')
+.addEventListener('click', function() {
+    document.querySelector('.question').textContent = '???'
+    document.querySelector('.question').style.width = '25rem';
+    document.querySelector('.guess-message').textContent = 'Начни угадывать'; 
+    document.querySelector('.score').textContent = score;
+    document.querySelector('body').style.backgroundColor = 'rgb(0, 0, 0)';
+    document.querySelector('.number-input').value = '';
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
 });
